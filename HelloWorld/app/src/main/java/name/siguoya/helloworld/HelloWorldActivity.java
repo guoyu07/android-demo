9182ee792261f0e6d31460fc0359cb6de7da0dd9
siguoya <924714558@qq.com>
2017-03-29 20:31:22 +0800

    编译文件的配置； 程序调试

--- HelloWorld/app/src/main/java/name/siguoya/helloworld/HelloWorldActivity.java
+++ HelloWorld/app/src/main/java/name/siguoya/helloworld/HelloWorldActivity.java
@@ -2,12 +2,19 @@ package name.siguoya.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+/**
+ * AppCompatActivity能够向下兼容Android以往的版本
+ * AppCompatActivity是Activity的子类，所有的活动类都需要继承自Activity类或它的子类
+ */
 public class HelloWorldActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("1234213","cccc");
         super.onCreate(savedInstanceState);
+        //Android程序的设计讲究逻辑和视图分离
         setContentView(R.layout.hello_world_layout);
     }
 }
