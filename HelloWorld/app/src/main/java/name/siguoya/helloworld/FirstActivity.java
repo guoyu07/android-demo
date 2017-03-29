d46f1f5c819f280ac783e13162d00fd1de364c3f
siguoya <924714558@qq.com>
2017-03-30 03:48:00 +0800

    活动显式跳转与隐式跳转； 活动scheme；

--- HelloWorld/app/src/main/java/name/siguoya/helloworld/FirstActivity.java
+++ HelloWorld/app/src/main/java/name/siguoya/helloworld/FirstActivity.java
@@ -1,6 +1,7 @@
 package name.siguoya.helloworld;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -39,8 +40,49 @@ public class FirstActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //活动跳转
-                Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
+                Intent intent=new Intent(FirstActivity.this, SecondActivity.class);
                 startActivity(intent);
+            }
+        });
+        Button btn5=(Button) findViewById(R.id.button_5);
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //活动跳转
+                Intent intent=new Intent("name.siguoya.helloworld.ACTION_START");
+                intent.addCategory("name.siguoya.helloworld.MY_CATEGORY");
+                startActivity(intent);
+            }
+        });
+        Button btn6=(Button) findViewById(R.id.button_6);
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //活动跳转
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com/"));
+                startActivity(intent);
+            }
+        });
+        Button btn7=(Button) findViewById(R.id.button_7);
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //活动跳转
+                Intent intent=new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:15521190637"));
+                startActivity(intent);
+            }
+        });
+        Button btn8=(Button) findViewById(R.id.button_8);
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //活动跳转
+                Intent intent=new Intent();
+                intent.setData(Uri.parse("baidumap://map/show?center=40.057406655722,116.29644071728&zoom=11&traffic=on&bounds=37.8608310000,112.5963090000,42.1942670000,118.9491260000"));
+                startActivity(intent);
+
             }
         });
     }
