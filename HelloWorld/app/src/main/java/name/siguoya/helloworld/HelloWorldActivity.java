a0940fee1b2057f2c11e87d47185175a9dc3b957
siguoya <924714558@qq.com>
2017-03-29 23:23:29 +0800

    便捷调试

--- HelloWorld/app/src/main/java/name/siguoya/helloworld/HelloWorldActivity.java
+++ HelloWorld/app/src/main/java/name/siguoya/helloworld/HelloWorldActivity.java
@@ -8,11 +8,16 @@ import android.util.Log;
  * AppCompatActivity能够向下兼容Android以往的版本
  * AppCompatActivity是Activity的子类，所有的活动类都需要继承自Activity类或它的子类
  */
+
 public class HelloWorldActivity extends AppCompatActivity {
+
+    //使用logt可以自动生成此行代码
+    private static final String TAG = "HelloWorldActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d("1234213","cccc");
+        //使用logd可以自动生成此行代码
+        Log.d(TAG, "onCreate: 调试");
         super.onCreate(savedInstanceState);
         //Android程序的设计讲究逻辑和视图分离
         setContentView(R.layout.hello_world_layout);
